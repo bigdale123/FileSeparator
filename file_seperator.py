62989ce52aa57a05c3ce325d1243990cf46957c5
bigdale123 <dylcal138@gmail.com>
2021-09-12 13:48:37 -0500

    Added ZIP support
    Program now supports unzipping files during sorting.  Cross Platform Functionality to come in next update.

--- file_seperator.py
+++ file_seperator.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+import zipfile
 
 def separator(path):
     for file in os.listdir(path):
@@ -11,6 +12,11 @@ def separator(path):
         else:
             os.mkdir(path+"\\"+name)
             shutil.move(path+"\\"+file, path+"\\"+name+"\\"+file)
+        if(file.endswith(".zip")):
+            with zipfile.ZipFile(path+"\\"+name+"\\"+file, 'r') as zip_ref:
+                zip_ref.extractall(path+"\\"+name+"\\"+file.split("_")[-1][:-4])
+        
+
 
 if __name__ == "__main__":
     for i in range(1,len(sys.argv)):
